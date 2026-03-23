b179963935bbb8c9ebb2d78f5ff0b523bb091db6
Rickard Karlberg <klbrg@users.noreply.github.com>
2026-03-23 18:59:21 +0100

    feat: make https the default scheme if not specified

--- main.go
+++ main.go
@@ -5,9 +5,22 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
 const version = "0.1.0"
+
+func buildRequest(method string, rawURL string) (*http.Request, error) {
+	if !strings.Contains(rawURL, "://") {
+		rawURL = "https://" + rawURL
+	}
+	req, err := http.NewRequest(method, rawURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("User-Agent", "gurl/"+version)
+	return req, nil
+}
 
 func run(args []string, stdout io.Writer, stderr io.Writer) int {
 	if len(args) < 2 {
@@ -27,20 +40,18 @@ func run(args []string, stdout io.Writer, stderr io.Writer) int {
 			fmt.Fprintln(stderr, "usage: gurl get <url>")
 			return 1
 		}
-		req, err = http.NewRequest(http.MethodGet, args[2], nil)
+		req, err = buildRequest(http.MethodGet, args[2])
 		if err != nil {
 			fmt.Fprintln(stderr, err)
 			return 1
 		}
 	default:
-		req, err = http.NewRequest(http.MethodGet, args[1], nil)
+		req, err = buildRequest(http.MethodGet, args[1])
 		if err != nil {
 			fmt.Fprintln(stderr, err)
 			return 1
 		}
 	}
-
-	req.Header.Set("User-Agent", "gurl/"+version)
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
